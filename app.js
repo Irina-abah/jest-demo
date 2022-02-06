48597ed96e49543ae43916a88ebec345fe8c7352
Irina Abah <irin.bon@gmail.com>
2022-02-06 00:05:35 +0000

    5 challenges done

--- app.js
+++ app.js
@@ -2,4 +2,38 @@ const add = (num1, num2) => {
   return num1 + num2;
  };
 
- module.exports = { add };
+ // challenges
+
+ //challenge 1
+
+const greeting = (name) => {
+  if (!name) {
+    return "Please provide with your name"; 
+  }
+  return { message: `Hello, ${name}!` };
+};
+
+// challenge 2 and 3
+
+const trueTask = () => {
+  return true;
+}
+
+// challenge 4
+
+const book = (title, author) => {
+  let bookItem = {
+    title: title,
+    author: author
+  }
+
+  return bookItem;
+}
+
+// challenge 6
+
+const NumToStr = (num) => {
+  return num.toString();
+}
+
+ module.exports = { add, greeting, trueTask, book, NumToStr };
--- tests/app.test.js
+++ tests/app.test.js
@@ -1,5 +1,27 @@
 const app = require("../app");
 
-test("should equal 5 when passed 2 and 3", ( ) => {
+test("should equal 5 when passed 2 and 3", () => {
   expect(app.add(2, 3)).toBe(5)
  });
+
+ test("should not be null", () => {
+   expect(app.greeting()).not.toBeNull();
+ });
+
+ test("should be truthy", () => {
+  expect(app.trueTask()).toBeTruthy();
+});
+
+test("should not be falthy", () => {
+  expect(app.trueTask()).not.toBeFalsy();
+});
+
+test("should have properties", () => {
+  expect(app.book("Domino", "Jack Jackson")).toHaveProperty("title", "Domino");
+  expect(app.book("Domino", "Jack Jackson")).toHaveProperty("author", "Jack Jackson");
+});
+
+test("should have properties", () => {
+  expect(app.NumToStr(5)).toBe("5");
+
+});
